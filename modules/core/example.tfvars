23d9337c745f13ee12dcbdc1d3f2efb67399e62c
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-03-29 13:38:59 +0800

    Fix incorrect URL

--- modules/core/example.tfvars
+++ modules/core/example.tfvars
@@ -12,3 +12,6 @@ hashi_ui_domain = "hashiui.example.com"
 nomad_api_domain = "nomad.example.com"
 traefik_ui_domain = "traefik-ui.example.com"
 consul_api_domain = "consul.example.com"
+vault_ami_id = "xxx"
+vault_allowed_inbound_cidr_blocks = ["0.0.0.0/0"]
+vault_tls_key_policy_arn = "xxx"
--- modules/core/vault.tf
+++ modules/core/vault.tf
@@ -4,7 +4,7 @@
 
 module "vault_cluster" {
     # source = "github.com/hashicorp/terraform-aws-consul.git/modules/vault-cluster?ref=v0.4.0"
-    source = "github.com/lawliet89/terraform-aws-consul.git/modules/vault-cluster?ref=aws_autoscaling_attachment"
+    source = "github.com/lawliet89/terraform-aws-vault.git//modules/vault-cluster?ref=aws_autoscaling_attachment"
 
     cluster_name  = "${var.vault_cluster_name}"
     cluster_size  = "${var.vault_cluster_size}"
