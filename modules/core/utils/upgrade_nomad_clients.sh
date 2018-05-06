bb86d5299e4684a0551e8bf1d8e51a1c748921db
sturdek <kwancheng.lai@gmail.com>
2018-05-06 14:38:11 +0800

    Start terminating instance once the node is drain

--- modules/core/utils/upgrade_nomad_clients.sh
+++ modules/core/utils/upgrade_nomad_clients.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/bash
 set -euo pipefail
 
-read -p 'AWS_PROFILE: ' awsProfile
-read -p 'NOMAD_ADDR: ' nomadAddr
-read -p 'NOMAD_TOKEN: ' nomadToken
-read -p 'ASGName: ' ASGName
+# Note: This script works assuming you are using the default configuration where the node name is the instance ID.
+# Below are the list of files that will be created when running the script:
+# instance-ids.txt store all the instance-ids of the ASG
+# nodes.json store the node infomation of all the nomad node, is use to get the node id of the nomad client
+# node-ids.txt store the node id of those nomad client that will be drain
 
-export AWS_PROFILE=$awsProfile
-export NOMAD_ADDR=$nomadAddr
-export NOMAD_TOKEN=$nomadToken
+readonly ASG_NAME="$1"
+readonly SLEEP_BETWEEN_RETRIES_SEC=10
 
 echo 'Getting old instance ID'
 aws autoscaling describe-auto-scaling-groups \
-    --auto-scaling-group-name $ASGName \
+    --auto-scaling-group-name $ASG_NAME \
     | jq --raw-output '.AutoScalingGroups[0].Instances[].InstanceId' \
     | tee instance-ids.txt
 
 echo 'Getting AutoScalingGroups max-size and desired-capacity'
 maxSize=$( aws autoscaling describe-auto-scaling-groups \
-    --auto-scaling-group-name $ASGName \
+    --auto-scaling-group-name $ASG_NAME \
     | jq --raw-output '.AutoScalingGroups[0].MaxSize' )
 
 desiredCapacity=$( aws autoscaling describe-auto-scaling-groups \
-    --auto-scaling-group-name $ASGName \
+    --auto-scaling-group-name $ASG_NAME \
     | jq --raw-output '.AutoScalingGroups[0].DesiredCapacity' )
 
-echo 'Increasing AutoScalingGroups max-size and desired-capacity by 2'
+echo 'Increasing AutoScalingGroups max-size and desired-capacity by two-fold'
 newMaxSize=$(($maxSize*2))
 newDesiredCapacity=$(($desiredCapacity*2))
 
@@ -34,16 +34,16 @@ echo "DesiredCapacity = $newDesiredCapacity"
 
 echo 'Updating AutoScalingGroups'
 aws autoscaling update-auto-scaling-group \
-    --auto-scaling-group-name $ASGName \
+    --auto-scaling-group-name $ASG_NAME \
     --max-size $newMaxSize \
     --desired-capacity $newDesiredCapacity
 
 echo 'Checking if new nodes are ready'
 nomad node status -json > nodes.json
-count=$( tr ' ' '\n' < nodes.json | grep -c ready )
+count=0
 while [[ $count -lt $newDesiredCapacity ]]; do
   echo "Waiting, currently only $count nodes are ready"
-  sleep 10 # can increase or decrease
+  sleep "$SLEEP_BETWEEN_RETRIES_SEC"
   nomad node status -json > nodes.json
   count=$( tr ' ' '\n' < nodes.json | grep -c ready )
 done
@@ -62,44 +62,42 @@ while read p; do
 done < node-ids.txt
 
 echo 'Draining old instances'
-while read a && read b <&3; do
+while read instance_id && read node_id <&3; do
   cont=true
-  echo "Detaching instance-ids ${a}"
+  echo "Detaching instance-ids ${instance_id}"
   while [ $cont != false ]; do
     errorMessage=$( aws autoscaling detach-instances --instance-ids ${p} \
-      --auto-scaling-group-name $ASGName \
+      --auto-scaling-group-name $ASG_NAME \
       --should-decrement-desired-capacity 2>&1 || echo $? )
     if echo $errorMessage | grep -q 'is not part of Auto Scaling group'; then
       cont=false
-      echo "Detaching instance-ids ${a} completed"
+      echo "Detaching instance-ids ${instance_id} completed"
       echo $errorMessage
     fi
-    echo "Still detaching instance-ids ${a}"
-    sleep 2
+    echo "Still detaching instance-ids ${instance_id}"
+    sleep "$SLEEP_BETWEEN_RETRIES_SEC"
   done
 
   drain=true
-  echo "Node drain for node-ids ${b}"
+  echo "Node drain for node-ids ${node_id}"
   while [ $drain != false ]; do
-    if nomad node drain -enable -yes ${b} | grep -q 'drain complete' then
+    if nomad node drain -enable -yes ${node_id} | grep -q 'drain complete' then
       drain=false
-      echo "Node drain complete for node-ids ${b}"
+      echo "Node drain complete for node-ids ${node_id}"
     fi
-    echo "Still draining node-ids ${b}"
-    sleep 10
+    echo "Still draining node-ids ${node_id}"
+    sleep "$SLEEP_BETWEEN_RETRIES_SEC"
   done
-done < instance-ids.txt 3<node-ids.txt
 
-echo 'Terminating old instances'
-aws ec2 terminate-instances \
-    --instance-ids $(cat instance-ids.txt | tr '\n' ' ')
-echo 'Termiation complete'
+  echo "Terminating instance: $instance_id "
+    aws ec2 terminate-instances \
+        --instance-ids ${instance_id}
+    echo 'Termiation complete'
+done < instance-ids.txt 3<node-ids.txt
 
 echo 'All operation complete'
 echo 'Clearing tempt files'
 rm instance-ids.txt nodes.json node-ids.txt
 
 echo 'Clearing exported env variable'
-unset AWS_PROFILE
-unset NOMAD_ADDR
-unset NOMAD_TOKEN
+
