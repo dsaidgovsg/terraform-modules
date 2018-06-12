dab5e227a20422a077402e0f97ff1c6d36f682b1
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-06-12 10:50:56 +0800

    Do not restrict server types
    Also enable selective telegraf enabling on specific type of servers

--- modules/telegraf/main.tf
+++ modules/telegraf/main.tf
@@ -1,9 +1,48 @@
-resource "consul_key_prefix" "core_integration" {
-  count       = "${var.core_integration ? 1 : 0}"
-  path_prefix = "${var.consul_key_prefix}telegraf/"
+resource "consul_keys" "core_integration" {
+  count = "${var.core_integration ? 1 : 0}"
 
-  subkeys {
-    "enabled" = "yes"
-    "README"  = "This is used for integration with the `core` module. See https://github.com/GovTechSG/terraform-modules/tree/master/modules/telegraf"
+  key {
+    path = "${var.consul_key_prefix}telegraf/README"
+
+    value = <<EOF
+This is used for integration with the `core` module.
+See https://github.com/GovTechSG/terraform-modules/tree/master/modules/telegraf
+EOF
+  }
+}
+
+resource "consul_keys" "consul" {
+  count = "${var.core_integration && var.consul_enabled ? 1 : 0}"
+
+  key {
+    path  = "${var.consul_key_prefix}telegraf/consul/enabled"
+    value = "true"
+  }
+}
+
+resource "consul_keys" "nomad_server" {
+  count = "${var.core_integration && var.nomad_server_enabled ? 1 : 0}"
+
+  key {
+    path  = "${var.consul_key_prefix}telegraf/nomad_server/enabled"
+    value = "true"
+  }
+}
+
+resource "consul_keys" "nomad_client" {
+  count = "${var.core_integration && var.nomad_client_enabled ? 1 : 0}"
+
+  key {
+    path  = "${var.consul_key_prefix}telegraf/nomad_client/enabled"
+    value = "true"
+  }
+}
+
+resource "consul_keys" "vault" {
+  count = "${var.core_integration && var.vault_enabled ? 1 : 0}"
+
+  key {
+    path  = "${var.consul_key_prefix}telegraf/vault/enabled"
+    value = "true"
   }
 }
