84e33e33d1f4a506e8bdd9cec9de56170a0ca865
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-06-27 14:16:46 +0800

    Upgrade to Consul 1.2
    Remove the beta UI environment variable

--- modules/core/user_data/user-data-consul-server.sh
+++ modules/core/user_data/user-data-consul-server.sh
@@ -16,8 +16,7 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 /opt/consul/bin/run-consul \
     --server \
     --cluster-tag-key "${cluster_tag_key}" \
-    --cluster-tag-value "${cluster_tag_value}" \
-    --environment "CONSUL_UI_BETA=\"true\""
+    --cluster-tag-value "${cluster_tag_value}"
 
 # Configure and run consul-template
 /opt/consul-template/bin/run-consul-template \
