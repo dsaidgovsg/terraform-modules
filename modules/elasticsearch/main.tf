7e8ebc4fbe1cd821945f29275f8bc23488a9c574
Chen Weiguang <chen.weiguang@gmail.com>
2018-07-25 09:38:06 +0800

    Revert back tf prefix to keep state sameness

--- modules/elasticsearch/main.tf
+++ modules/elasticsearch/main.tf
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "es_resource_attached_policy" {
 }
 
 resource "aws_elasticsearch_domain" "es" {
-  domain_name           = "${var.es_domain_name}"
+  domain_name           = "tf-${var.es_domain_name}"
   elasticsearch_version = "${var.es_version}"
 
   cluster_config {
@@ -92,7 +92,7 @@ resource "aws_elasticsearch_domain" "es" {
 }
 
 resource "aws_elasticsearch_domain_policy" "es_resource_attached_policy" {
-  domain_name     = "${var.es_domain_name}"
+  domain_name     = "tf-${var.es_domain_name}"
   access_policies = "${data.aws_iam_policy_document.es_resource_attached_policy.json}"
 }
 
