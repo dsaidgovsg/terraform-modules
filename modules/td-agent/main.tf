bbc451499b159f8c124febeeaa50a4551d67de4d
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-08-02 15:34:01 +0800

    Format Terraform

--- modules/td-agent/main.tf
+++ modules/td-agent/main.tf
@@ -2,7 +2,7 @@ resource "consul_keys" "core_integration" {
   count = "${var.core_integration ? 1 : 0}"
 
   key {
-    path = "${var.consul_key_prefix}td-agent/README"
+    path   = "${var.consul_key_prefix}td-agent/README"
     delete = true
 
     value = <<EOF
@@ -16,8 +16,8 @@ resource "consul_keys" "consul" {
   count = "${var.core_integration && var.consul_enabled ? 1 : 0}"
 
   key {
-    path  = "${var.consul_key_prefix}td-agent/consul/enabled"
-    value = "yes"
+    path   = "${var.consul_key_prefix}td-agent/consul/enabled"
+    value  = "yes"
     delete = true
   }
 }
@@ -26,8 +26,8 @@ resource "consul_keys" "nomad_server" {
   count = "${var.core_integration && var.nomad_server_enabled ? 1 : 0}"
 
   key {
-    path  = "${var.consul_key_prefix}td-agent/nomad_server/enabled"
-    value = "yes"
+    path   = "${var.consul_key_prefix}td-agent/nomad_server/enabled"
+    value  = "yes"
     delete = true
   }
 }
@@ -36,8 +36,8 @@ resource "consul_keys" "nomad_client" {
   count = "${var.core_integration && var.nomad_client_enabled ? 1 : 0}"
 
   key {
-    path  = "${var.consul_key_prefix}td-agent/nomad_client/enabled"
-    value = "yes"
+    path   = "${var.consul_key_prefix}td-agent/nomad_client/enabled"
+    value  = "yes"
     delete = true
   }
 }
@@ -46,8 +46,8 @@ resource "consul_keys" "vault" {
   count = "${var.core_integration && var.vault_enabled ? 1 : 0}"
 
   key {
-    path  = "${var.consul_key_prefix}td-agent/vault/enabled"
-    value = "yes"
+    path   = "${var.consul_key_prefix}td-agent/vault/enabled"
+    value  = "yes"
     delete = true
   }
 }
