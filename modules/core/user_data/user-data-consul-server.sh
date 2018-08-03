cef93940a370a6bd7a400130f555e9764ec47189
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-08-03 09:18:46 +0800

    Report User Data Failure in Consul Agents only (#131)
    * Create a marker file on initialisation completion
    
    And do a check in Consul
    
    * Fix marker_path templating issue
    
    * Fix script error
    
    * Fix typo

--- modules/core/user_data/user-data-consul-server.sh
+++ modules/core/user_data/user-data-consul-server.sh
@@ -3,10 +3,11 @@
 # run-consul script to configure and start Consul in server mode. Note that this script assumes it's running in an AMI
 # built from the Packer template in examples/consul-ami/consul.json in the Consul AWS Module.
 
-set -e
+set -euo pipefail
 
 # Avoid Terraform template by either using double dollar signs, or not using curly braces
 readonly service_type="consul"
+readonly marker_path="/etc/user-data-marker"
 
 # Send the log output from this script to user-data.log, syslog, and the console
 # From: https://alestic.com/2010/12/ec2-user-data-output/
@@ -21,6 +22,7 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 # Post startup Configuration
 /opt/consul/bin/post-configure \
     --server \
+    --initialisation-marker-path "$marker_path" \
     --consul-prefix "${consul_prefix}"
 
 # Configure and run consul-template
@@ -41,3 +43,6 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 /opt/run-telegraf \
     --consul-prefix "${consul_prefix}" \
     --type "$service_type"
+
+# Touch the marker file to indicate completion
+touch "$marker_path"
--- modules/core/user_data/user-data-nomad-client.sh
+++ modules/core/user_data/user-data-nomad-client.sh
@@ -4,10 +4,11 @@
 # in client mode. Note that this script assumes it's running in an AMI built from the Packer template in
 # examples/nomad-consul-ami/nomad-consul.json.
 
-set -e
+set -euo pipefail
 
 # Avoid Terraform template by either using double dollar signs, or not using curly braces
 readonly service_type="${service_type}"
+readonly marker_path="/etc/user-data-marker"
 
 # Send the log output from this script to user-data.log, syslog, and the console
 # From: https://alestic.com/2010/12/ec2-user-data-output/
@@ -22,6 +23,7 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 # Post startup Configuration
 /opt/consul/bin/post-configure \
     --client \
+    --initialisation-marker-path "$marker_path" \
     --consul-prefix "${consul_prefix}"
 
 # Configure and run consul-template
@@ -49,3 +51,6 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 /opt/run-telegraf \
     --consul-prefix "${consul_prefix}" \
     --type "$service_type"
+
+# Touch the marker file to indicate completion
+touch "$marker_path"
