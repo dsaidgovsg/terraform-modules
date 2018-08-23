33d7de9eba7f13aaffd0b0c960f10203b5d3ac22
Chen Weiguang <chen.weiguang@gmail.com>
2018-08-23 11:00:18 +0800

    Shift comments to README

--- modules/vault-app-policy/main.tf
+++ modules/vault-app-policy/main.tf
@@ -1,16 +1,3 @@
-#################################################
-# Policies for apps to access secrets in Vault's KV Store
-# Generate two policies that affect the KV path at "${var.kv_path}/app/${var.app}"
-# The `app` policy will restrict to only readonly access
-# The `dev` policy will allow read/write access.
-#
-# The policies will have the following naming convention:
-# - Read-only  (app): "${var.prefix}_${var.app}_app"
-# - Read-Write (dev): "${var.prefix}_${var.app}_dev"
-#
-# Be sure not to use same app name across all your application services
-#################################################
-
 data "template_file" "app_readonly" {
   template = "${file("${path.module}/templates/kv_app.hcl")}"
 
