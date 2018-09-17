a120ecd0341181fb039813989142a10b239b3cf0
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-09-17 17:22:30 +0800

    Add more outputs to the `fluentd` module

--- modules/fluentd/outputs.tf
+++ modules/fluentd/outputs.tf
@@ -2,3 +2,13 @@ output "jobspec" {
   description = "Rendered jobspec"
   value       = "${data.template_file.fluentd_jobspec.rendered}"
 }
+
+output "s3_arn" {
+  description = "ARN of the S3 bucket created"
+  value       = "${var.logs_s3_enabled ? aws_s3_bucket.logs.arn : ""}"
+}
+
+output "s3_iam_arn" {
+  description = "ARN of the IAM Policy document for S3 access"
+  value       = "${var.logs_s3_enabled ? aws_iam_policy.logs_s3.arn : ""}"
+}
