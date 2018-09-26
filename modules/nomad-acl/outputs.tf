9d17a996b1659cd6680b81f11ae9f56775a05f68
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-09-26 13:37:42 +0800

    Fix formatting

--- modules/nomad-acl/outputs.tf
+++ modules/nomad-acl/outputs.tf
@@ -1,4 +1,4 @@
 output "path" {
   description = "Path to the Nomad secrets engine. Useful for implicit dependencies"
-  value = "${vault_mount.nomad.path}"
+  value       = "${vault_mount.nomad.path}"
 }
