1047d1116c1e58bda540b5d8614e0b7fd722fb23
qbiqing <bq2134@columbia.edu>
2020-03-10 16:12:26 +0800

    set up fluentd_new, make ASG, fluentd_new can plan

---
+++ modules/fluentd_new/elb.tf
@@ -0,0 +1,131 @@
+locals {
+  private_zone_id     = join("", aws_route53_zone.private.*.zone_id)
+  fluentd_server_port = 4224
+  fluentd_lb_port     = var.fluentd_port
+}
+
+##############################
+# Define an internal load balancer
+##############################
+
+# Internal Load balancer
+resource "aws_lb" "fluentd" {
+  name               = var.lb_name
+  subnets            = var.lb_subnets
+  internal           = true
+  load_balancer_type = "network"
+
+  idle_timeout               = var.lb_idle_timeout
+  enable_deletion_protection = true
+
+  access_logs {
+    enabled = var.lb_access_log
+    bucket  = var.lb_access_log_bucket
+    prefix  = var.lb_access_log_prefix
+  }
+
+  tags = merge(var.lb_tags, { Name = var.lb_name })
+}
+
+resource "aws_lb_listener" "fluentd_tcp" {
+  load_balancer_arn = aws_lb.fluentd.arn
+  port              = local.fluentd_lb_port
+  protocol          = "TCP"
+
+  # Redirect to HTTPS
+  default_action {
+    target_group_arn = aws_lb_target_group.fluentd_server.arn
+    type             = "forward"
+  }
+}
+
+# Security group for the Internal LB
+resource "aws_security_group" "fluentd_lb" {
+  name        = var.lb_name
+  description = "Security group for Internal Load balancer for Fluentd"
+  vpc_id      = var.vpc_id
+
+  tags = merge(var.lb_tags, { Name = var.lb_name })
+}
+
+resource "aws_security_group_rule" "fluentd_lb_incoming" {
+  type              = "ingress"
+  security_group_id = aws_security_group.fluentd_lb.id
+  from_port         = local.fluentd_lb_port
+  to_port           = local.fluentd_lb_port
+  protocol          = "tcp"
+  cidr_blocks       = concat(var.allowed_inbound_cidr_blocks, var.lb_incoming_cidr)
+}
+
+resource "aws_lb_target_group" "fluentd_server" {
+  name_prefix          = "fluent"
+  port                 = local.fluentd_server_port
+  protocol             = "TCP"
+  vpc_id               = var.vpc_id
+  deregistration_delay = var.fluentd_server_lb_deregistration_delay
+
+  health_check {
+    healthy_threshold   = var.lb_healthy_threshold
+    unhealthy_threshold = var.lb_unhealthy_threshold
+    port                = local.fluentd_server_port
+    interval            = var.lb_health_check_interval
+  }
+
+  tags = merge(var.lb_tags, { Name = var.tg_group_name })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Attach target group to the Fluentd servers ASG
+resource "aws_autoscaling_attachment" "fluentd_server_internal" {
+  autoscaling_group_name = var.asg_name
+  alb_target_group_arn   = aws_lb_target_group.fluentd_server.arn
+}
+
+resource "aws_security_group_rule" "fluentd_outgoing" {
+  type                     = "egress"
+  security_group_id        = aws_security_group.fluentd_lb.id
+  from_port                = local.fluentd_server_port
+  to_port                  = local.fluentd_server_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.lc_security_group.id
+}
+
+# Security rules for Fluentd servers to be accessible by the internal LB
+resource "aws_security_group_rule" "fluentd_to_lb" {
+  type                     = "ingress"
+  security_group_id        = aws_security_group.lc_security_group.id
+  from_port                = local.fluentd_server_port
+  to_port                  = local.fluentd_server_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.fluentd_lb.id
+}
+
+# A Record for endpoint to point to Internal Load balancer
+resource "aws_route53_record" "fluentd_rpc" {
+  zone_id = data.aws_route53_zone.default.zone_id
+  name    = var.fluentd_api_domain
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.fluentd.dns_name
+    zone_id                = aws_lb.fluentd.zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "private_zone_fluentd_rpc" {
+  count = var.add_private_route53_zone ? 1 : 0
+
+  zone_id = local.private_zone_id
+  name    = var.fluentd_api_domain
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.fluentd.dns_name
+    zone_id                = aws_lb.fluentd.zone_id
+    evaluate_target_health = false
+  }
+}
---
+++ modules/fluentd_new/sg.tf
@@ -0,0 +1,89 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Adapted from HashiCorp implementation of a Nomad Cluster 
+# https://github.com/hashicorp/terraform-aws-nomad//modules/nomad-security-group-rules
+# 
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE A SECURITY GROUP TO CONTROL WHAT REQUESTS CAN GO IN AND OUT OF EACH EC2 INSTANCE
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "lc_security_group" {
+  name_prefix = var.cluster_name
+  description = "Security group for the ${var.cluster_name} launch configuration"
+  vpc_id      = var.vpc_id
+
+  # aws_launch_configuration.launch_configuration in this module sets create_before_destroy to true, which means
+  # everything it depends on, including this resource, must set it as well, or you'll get cyclic dependency errors
+  # when you try to do a terraform destroy.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  count       = length(var.allowed_inbound_cidr_blocks) >= 1 ? 1 : 0
+  type        = "ingress"
+  from_port   = var.http_port
+  to_port     = var.http_port
+  protocol    = "tcp"
+  cidr_blocks = var.allowed_inbound_cidr_blocks
+
+  security_group_id = aws_security_group.lc_security_group.id
+}
+
+resource "aws_security_group_rule" "allow_rpc_inbound" {
+  count       = length(var.allowed_inbound_cidr_blocks) >= 1 ? 1 : 0
+  type        = "ingress"
+  from_port   = var.rpc_port
+  to_port     = var.rpc_port
+  protocol    = "tcp"
+  cidr_blocks = var.allowed_inbound_cidr_blocks
+
+  security_group_id = aws_security_group.lc_security_group.id
+}
+
+resource "aws_security_group_rule" "allow_serf_tcp_inbound" {
+  count       = length(var.allowed_inbound_cidr_blocks) >= 1 ? 1 : 0
+  type        = "ingress"
+  from_port   = var.serf_port
+  to_port     = var.serf_port
+  protocol    = "tcp"
+  cidr_blocks = var.allowed_inbound_cidr_blocks
+
+  security_group_id = aws_security_group.lc_security_group.id
+}
+
+resource "aws_security_group_rule" "allow_serf_udp_inbound" {
+  count       = length(var.allowed_inbound_cidr_blocks) >= 1 ? 1 : 0
+  type        = "ingress"
+  from_port   = var.serf_port
+  to_port     = var.serf_port
+  protocol    = "udp"
+  cidr_blocks = var.allowed_inbound_cidr_blocks
+
+  security_group_id = aws_security_group.lc_security_group.id
+}
+
+resource "aws_security_group_rule" "allow_ssh_inbound" {
+  type        = "ingress"
+  from_port   = var.ssh_port
+  to_port     = var.ssh_port
+  protocol    = "tcp"
+  cidr_blocks = var.allowed_ssh_cidr_blocks
+
+  security_group_id = aws_security_group.lc_security_group.id
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.lc_security_group.id
+}
