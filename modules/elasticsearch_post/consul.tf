7c3ad47632c401ecb8fcb2ebd81acc0b0a284df3
Chen Weiguang <chen.weiguang@gmail.com>
2020-10-23 11:52:33 +0800

    feat(elasticsearch): add Consul service health check

--- modules/elasticsearch_post/consul.tf
+++ modules/elasticsearch_post/consul.tf
@@ -7,4 +7,15 @@ resource "consul_service" "es" {
   node = consul_node.es.name
   name = var.es_consul_service
   port = var.es_default_access["port"]
+
+  check {
+    check_id                          = "service:elasticsearch"
+    name                              = "Elasticsearch cluster health check"
+    status                            = "passing"
+    http                              = "https://${var.es_endpoint}/_cluster/health"
+    tls_skip_verify                   = false
+    interval                          = "30s"
+    timeout                           = "10s"
+    deregister_critical_service_after = "600s"
+  }
 }
