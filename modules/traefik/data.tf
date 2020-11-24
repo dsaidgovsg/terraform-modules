79f1ce72a2f9f4d61277bcc96c246918fc01ca00
Chen Weiguang <chen.weiguang@gmail.com>
2020-11-24 14:37:55 +0800

    fix(traefik): remove vpc single cidr assumption
    This makes the module not work properly in VPC with multiple CIDR
    values.
    
    BREAKING CHANGE: users of this module needs to now manually add in their
    vpc cidr in internal_lb_incoming_cidr if they were relying on it.

--- modules/traefik/data.tf
+++ modules/traefik/data.tf
@@ -2,10 +2,6 @@ data "aws_route53_zone" "default" {
   name = "${var.route53_zone}."
 }
 
-data "aws_vpc" "traefik" {
-  id = var.vpc_id
-}
-
 data "aws_region" "current" {}
 
 data "aws_availability_zones" "available" {}
