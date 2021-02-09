cd99db0da71b97167c2945c34e2208adc06ba532
Weiguang <chen.weiguang@gmail.com>
2021-02-09 10:57:27 +0800

    feat(elasticsearch): add Consul service health check (#276)
    Add meta and touch up settings to allow Consul ESM to operate if available.

--- modules/elasticsearch_post/consul.tf
+++ modules/elasticsearch_post/consul.tf
@@ -1,10 +1,27 @@
 resource "consul_node" "es" {
   name    = var.es_consul_service
   address = var.es_endpoint
+
+  meta = {
+    "external-node"  = "true"
+    "external-probe" = "true"
+  }
 }
 
 resource "consul_service" "es" {
   node = consul_node.es.name
   name = var.es_consul_service
   port = var.es_default_access["port"]
+  tags = ["elasticsearch"]
+
+  check {
+    check_id                          = "service:elasticsearch"
+    name                              = "Elasticsearch cluster health check"
+    status                            = "passing"
+    http                              = "${var.es_endpoint}/_cluster/health"
+    method                            = "GET"
+    interval                          = "30s"
+    timeout                           = "10s"
+    deregister_critical_service_after = "600s"
+  }
 }
