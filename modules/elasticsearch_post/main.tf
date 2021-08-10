d758091c87ae3c72c94e04447e0bfecce791801f
qbiqing <bq2134@columbia.edu>
2021-08-10 15:43:48 +0800

    remove elasticsearch_post

--- modules/elasticsearch_post/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_security_group_rule" "es_post_access_rule" {
-  type              = var.es_default_access["type"]
-  from_port         = var.es_default_access["port"]
-  to_port           = var.es_default_access["port"]
-  protocol          = var.es_default_access["protocol"]
-  cidr_blocks       = var.es_post_access_cidr_block
-  security_group_id = var.es_security_group_id
-}
