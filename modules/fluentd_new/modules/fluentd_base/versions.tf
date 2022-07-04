abc15fd3a08d9c7cecdfc7a35316c01ff0191421
Becca Qiu <bq2134@columbia.edu>
2022-07-04 16:47:40 +0800

    chore(fluentd): remove fluentd_pre and fluentd_new (#291)
    Remove because outdated and the single fluentd setup is easier to use.

--- modules/fluentd_new/modules/fluentd_base/versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 2.42, < 4.0.0"
-    }
-  }
-}
--- modules/fluentd_new/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "security_group_id" {
-  description = "Security Group ID for Fluentd servers"
-  value       = aws_security_group.lc_security_group.id
-}
