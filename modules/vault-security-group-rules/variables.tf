bf4fe37f7fadca4723ae9ff7f817dc72b2cb4ce2
tingweiftw <tingwei.f@gmail.com>
2023-12-01 10:36:06 +0800

    add in vault-security-group

---
+++ modules/vault-security-group-rules/variables.tf
@@ -0,0 +1,38 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "security_group_id" {
+  description = "The ID of the security group to which we should add the Vault security group rules"
+}
+
+variable "allowed_inbound_cidr_blocks" {
+  description = "A list of CIDR-formatted IP address ranges from which the EC2 Instances will allow connections to Vault"
+  type        = list(string)
+}
+
+variable "allowed_inbound_security_group_ids" {
+  description = "A list of security group IDs that will be allowed to connect to Vault"
+  type        = list(string)
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "allowed_inbound_security_group_count" {
+  description = "The number of entries in var.allowed_inbound_security_group_ids. Ideally, this value could be computed dynamically, but we pass this variable to a Terraform resource's 'count' property and Terraform requires that 'count' be computed with literals or data sources only."
+}
+
+variable "api_port" {
+  description = "The port to use for Vault API calls"
+  default     = 8200
+}
+
+variable "cluster_port" {
+  description = "The port to use for Vault server-to-server communication"
+  default     = 8201
+}
+
