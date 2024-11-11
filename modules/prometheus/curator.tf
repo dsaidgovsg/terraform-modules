271da04339fab178f78a6984789bc20b1fce8227
Fan Ting Wei <tingwei@dsaid.gov.sg>
2024-11-11 15:23:56 +0800

    chore(prometheus): unsafe deserialisation (#300)
    * change submodule to directory
    
    * use safe load
    
    * remove prometheus
    
    * remove prometheus module

--- modules/prometheus/curator.tf
+++
@@ -1,9 +0,0 @@
-module "curator" {
-  source = "../curator/action"
-
-  key               = "prometheus"
-  disable           = var.curator_enable ? "false" : "true"
-  age               = var.curator_age
-  prefix            = var.curator_prefix
-  consul_key_prefix = var.consul_key_prefix
-}
